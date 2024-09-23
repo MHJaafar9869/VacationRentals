04291ed182515a7dd668191adf586ba645c0347f
MHJaafar9869 <mostafajaafar9869@gmail.com>
2024-09-23 18:22:19 +0300

    Property Routes & API setup for CRUD operations

---
+++ app/Http/Resources/PropertyResource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PropertyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "headline" => $this->headline,
+            "description" => $this->description,
+            "amenities" => $this->amenities,
+            "number of rooms" => $this->number_of_rooms,
+            "image" => $this->image,
+            "city" => $this->city,
+            "country" => $this->country,
+            "address" => $this->address,
+            "night rate" => $this->night_rate,
+            "status" => $this->status,
+            "created at" => $this->created_at,
+            "modified at" => $this->updated_at,
+
+        ];
+    }
+}
