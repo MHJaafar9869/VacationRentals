fb460f4646a919995cfd3ffb4ffdae086007cd44
“hamo4651” <hamo4651@gmail.com>
2024-09-23 16:12:47 +0300

    categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
-
+     protected $fillable = [
+        'name'
+     ];
     public function properties()
     {
         return $this->hasMany(Property::class);
