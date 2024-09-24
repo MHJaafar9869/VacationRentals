b9edf90df1eae27144758053ad3e64cacd21e1c4
“hamo4651” <hamo4651@gmail.com>
2024-09-24 23:53:14 +0300

    forget_reset_password

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminAuth\PasswordResetController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PropertyController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
@@ -33,6 +34,9 @@ Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->midd
 Route::post('/register/owner', [App\Http\Controllers\AdminAuth\RegisteredUserController::class, 'store']);
 Route::post('/login/owner', [App\Http\Controllers\AdminAuth\AuthenticatedSessionController::class, 'store']);
 Route::post('/logout/owner', [App\Http\Controllers\AdminAuth\AuthenticatedSessionController::class, 'destroy'])->middleware('auth:sanctum');
+Route::post('owners/password/email', [PasswordResetController::class, 'sendResetLinkEmail']);
+Route::post('owners/password/reset', [PasswordResetController::class, 'reset']);
+
 // ===================End Owner Routes====================
 
 // ===================Edit profile Routes====================
