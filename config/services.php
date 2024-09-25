85427cf8b483e6316e01155ae68ca0778fdae75b
MHJaafar9869 <mostafajaafar9869@gmail.com>
2024-09-25 17:07:14 +0300

    Stripe Setup

--- config/services.php
+++ config/services.php
@@ -28,6 +28,10 @@ return [
         'key' => env('RESEND_KEY'),
     ],
 
+    'stripe' => [
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
     'slack' => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
