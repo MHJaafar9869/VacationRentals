a205c23170092d566923a83f78518d39e1a2930c
“hamo4651” <hamo4651@gmail.com>
2024-10-01 21:16:01 +0300

    forget_password

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Owner;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,8 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+        //     return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        // });
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            $userType = $notifiable instanceof Owner ? 'owners' : 'users'; // تأكد من أن لديك كلاس Owner
+    
+            return config('app.frontend_url') . "/password-reset/$userType/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
     }
 }
